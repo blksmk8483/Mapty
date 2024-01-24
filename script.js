98c24841ed1645f4bf1a905654ba208d80323d57
Brandon Knight <knightbrandon1984@gmail.com>
2024-01-23 23:45:55 -0600

    creating a new workout

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
-
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
@@ -59,9 +51,19 @@ class Cycling extends Workout {
 // ========   Application   ========
 // =================================
 
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
+
   constructor() {
     this._getPosition();
     form.addEventListener('submit', this._netWorkout.bind(this));
@@ -114,17 +116,56 @@ class App {
 
   // =========   FORM SUBMISSION   =========
   _netWorkout(e) {
+    // validInputs is a helper function that uses the rest operator to loop through (distance, duration, cadence) to validate whether or not they are Numbers
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
 
-    // Clearing the input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    // Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    // If workout is running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      // Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    // If workout is cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+    // Add new ojbect to workout array
+    this.#workouts.push(workout);
+    console.log(workout);
+
+    // Render workout on map as a marker
 
     // Display Marker
-    const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -138,7 +179,61 @@ class App {
       )
       .setPopupContent('Workout')
       .openPopup();
+
+    // Render workout on list
+
+    // Hide the form + Clearing the input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
   }
 }
 
 const app = new App();
+
+// ==============
+// This is a great example of using the rest operator ...
+// This gives me validation through the helper functions at the top and allows me to make it dynamic
+// ==============
+
+// // ======  helper functions  ======
+
+// // so this one checks if the input is a number
+// const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+
+// // this checks if the number is a positive number
+// const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+// e.preventDefault();
+
+// // Get data from form
+// const type = inputType.value;
+// const distance = +inputDistance.value;
+// const duration = +inputDuration.value;
+
+// // If workout is running, create running object
+// if (type === 'running') {
+//   const cadence = +inputCadence.value;
+//   // Check if data is valid
+//   if (
+//     // !Number.isFinite(distance) ||
+//     // !Number.isFinite(duration) ||
+//     // !Number.isFinite(cadence)
+//     !validInputs(distance, duration, cadence) ||
+//     !allPositive(distance, duration, cadence)
+//   )
+//     return alert('Inputs have to be positive numbers!');
+// }
+
+// // If workout is cycling, create cycling object
+// if (type === 'cycling') {
+//   const elevation = +inputElevation.value;
+
+//   if (
+//     !validInputs(distance, duration, elevation) ||
+//     !allPositive(distance, duration)
+//   )
+//     return alert('Inputs have to be positive numbers!');
+// }
